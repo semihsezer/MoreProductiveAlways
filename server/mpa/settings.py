2f5761d49261a31e614a5ae52e121a2e5ab433c5
semihsezer <semih.c.sezer@gmail.com>
2024-10-11 22:15:10 +0100

    Change proxy setup

--- server/mpa/settings.py
+++ server/mpa/settings.py
@@ -185,6 +185,7 @@ LOGIN_REDIRECT_URL = "/"
 CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
+    "https://mpa-ui-production.up.railway.app",
 ]
 
 # Django Rest Auth
