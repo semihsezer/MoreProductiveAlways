27429b95a21c9ee6d18236b2fa1228caa54db98c
semihsezer <semih.c.sezer@gmail.com>
2024-10-12 20:35:59 +0100

    Add github action

--- frontend_react/src/setupProxy.js
+++ frontend_react/src/setupProxy.js
@@ -1,5 +1,5 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const REACT_APP_API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
+const REACT_APP_BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:8000";
 
 module.exports = function (app) {
   app.use(
